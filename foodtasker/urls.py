bf7639a6fbbc3c722db5fc6fc049eeceefe0b33c
jose <jose_redc@hotmail.com>
2019-07-14 20:19:26 -0700

    registration form

--- foodtasker/urls.py
+++ foodtasker/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import url
 from django.contrib import admin
 from foodtaskerapp import views
 from django.contrib.auth import views as auth_views
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -20,4 +23,5 @@ urlpatterns = [
 
      url(r'^restaurant/$', views.restaurant_home,
         name = 'restaurant-home'),
-]
+        
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
